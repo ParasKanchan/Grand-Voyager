dc223a1a94c60bb250f56b9f67b9702d3cd8301a
ParasKanchan <paraskanchan438@gmail.com>
2025-07-26 00:29:14 +0545

    debugging

--- app.js
+++ app.js
@@ -17,18 +17,12 @@ const reviewRouter = require("./routes/reviews.js");
 const userRouter = require("./routes/user.js");
 require('dotenv').config();
 
-// async function main() {
-//     await mongoose.connect("mongodb://localhost:27017/GrandVoyager");
-// }
-// main().then(() => { console.log("Connected to DB"); }).catch(err => { console.log(err); });
-
 const dbURL = process.env.ATLAS_URL;
 
- main().then(() => { console.log("Connected to DB"); }).catch(err => { console.log(err); });
- async function main() {
-     await mongoose.connect(dbURL);
- }
-
+main().then(() => { console.log("Connected to DB"); }).catch(err => { console.log(err); });
+async function main() {
+    await mongoose.connect(dbURL);
+}
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -37,86 +31,74 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-// app.get("/", (req, res) => {
-//     res.send("Hi, I am root");
-// });
+// ✅ BONUS TIP: Handle favicon.ico request to avoid 500 error
+app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 const store = MongoStore.create({
-    mongoUrl : dbURL,
-    crypto : {
-        secret : process.env.SECTRET
+    mongoUrl: dbURL,
+    crypto: {
+        secret: process.env.SECTRET
     },
-    touchAfter : 24 * 3600 //session apne aap se kab update ho jaaega
-})
+    touchAfter: 24 * 3600
+});
 
-store.on("error",()=>{
-    console.log("Error in MONGO SESSION STORE ",err)
-})
+store.on("error", (err) => {
+    console.log("Error in MONGO SESSION STORE ", err);
+});
 
 const sessionOptions = {
     store,
-    secret : process.env.SECTRET,
-    resave : false,
-    saveUninitialized : true,
-    cookie : {
-        expires : Date.now() + 7 *24 * 60 * 60 *1000,//After this time cookie will expire---Date.now()gives time in milisecond
-        maxAge : 7 *24 * 60 * 60 *1000,
-        httpOnly : true,//for security purposes
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
     }
-}
+};
 
 app.use(session(sessionOptions));
 app.use(flash());
 
-//Configuring strategy for using passport as a middleware
-//hasing algorithm implemeted is "pbkdf2"
 app.use(passport.initialize());
-app.use(passport.session());//every request should know that it is the part of which session--to handle this we used this middleware
+app.use(passport.session());
 
-//use static authenticate method of model in "LocalStrategy"
-passport.use(new LocalStrategy(User.authenticate()))
-
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
-    //To access the user in ejs template of signup login & logout
-    res.locals.currUser = req.user
+    res.locals.currUser = req.user;
     next();
-})
+});
 
-app.get("/demouser", async(req,res)=>{
+app.get("/demouser", async (req, res) => {
     let fakeUser = new User({
-        email : "std1@gmail.com",
-        username : "delta1"//this username will override the username given by passport-local-mongoose
-    })
+        email: "std1@gmail.com",
+        username: "delta1"
+    });
 
-    //to register a new user instance with a given instance
-   let registeredUser = await User.register(fakeUser,"helloworld");
-    //here helloworld is a password
+    let registeredUser = await User.register(fakeUser, "helloworld");
     res.send(registeredUser);
 });
 
 // Mounting Routes
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", (req, res, next) => {
-    req.listingId = req.params.id; // Pass the ID to the router
+    req.listingId = req.params.id;
     next();
 }, reviewRouter);
-app.use("/",userRouter);
+app.use("/", userRouter);
 
-// Handling unknown routes
-
+// Catch-All for Undefined Routes
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page not Found!!"));
 });
 
-
-
-// Error Handling Middleware
+// Error Handler Middleware
 app.use((err, req, res, next) => {
     let { statuscode = 500, message = "Something Went Wrong!" } = err;
     res.status(statuscode).render("error.ejs", { message });
